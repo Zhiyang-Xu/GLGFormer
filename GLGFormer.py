8082a8a0765cde520cfb4822b2e8efaa8afcceb4
zhiyang xu <63634194+xzy-create@users.noreply.github.com>
2023-03-04 15:20:29 +0800

    Update GLGFormer.py

--- GLGFormer.py
+++ GLGFormer.py
@@ -65,9 +65,9 @@ class glgformer(nn.Module):
         # self.aa_2 = AA_()
         # self.aa_3 = AA_()
 
-        self.aa_kernel_1 = PSA_p(32, 32)
-        self.aa_kernel_2 = PSA_p(32, 32)
-        self.aa_kernel_3 = PSA_p(32, 32)
+        self.aa_kernel_1 = ggm(32, 32)
+        self.aa_kernel_2 = ggm(32, 32)
+        self.aa_kernel_3 = ggm(32, 32)
         # self.aa_kernel_1 = AA_kernel(256, 256)
         # self.aa_kernel_2 = AA_kernel(256, 256)
         # self.aa_kernel_3 = AA_kernel(256, 256)
@@ -332,9 +332,9 @@ def kaiming_init(module,
         nn.init.constant_(module.bias, bias)
 
 
-class PSA_p(nn.Module):
+class ggm(nn.Module):
     def __init__(self, inplanes, planes, kernel_size=1, stride=1):
-        super(PSA_p, self).__init__()
+        super(ggm, self).__init__()
 
         self.inplanes = inplanes
         self.inter_planes = planes // 2
@@ -428,7 +428,7 @@ class PSA_p(nn.Module):
 
         # [N, IC, H*W]
         theta_x = self.conv_v_left(x)
-        map = (torch.sigmoid(map))
+        map = torch.sigmoid(map)
         theta_x = theta_x * map
         # theta_x = self.conv_v_left(x).view(batch, self.inter_planes, height * width)
         theta_x = theta_x.view(batch, self.inter_planes, height * width)
@@ -473,239 +473,6 @@ class PSA_p(nn.Module):
         return out
 
 
-# class PSA_p(nn.Module):
-#     def __init__(self, inplanes, planes, kernel_size=1, stride=1):
-#         super(PSA_p, self).__init__()
-#
-#         self.inplanes = inplanes
-#         self.inter_planes = planes // 2
-#         self.planes = planes
-#         self.kernel_size = kernel_size
-#         self.stride = stride
-#         self.padding = (kernel_size-1)//2
-#
-#         self.conv_q_right = nn.Conv2d(self.inplanes, 1, kernel_size=1, stride=stride, padding=0, bias=False)
-#         self.conv_v_right = nn.Conv2d(self.inplanes, self.inter_planes, kernel_size=1, stride=stride, padding=0, bias=False)
-#         self.conv_up = nn.Conv2d(self.inter_planes, self.planes, kernel_size=1, stride=1, padding=0, bias=False)
-#         self.softmax_right = nn.Softmax(dim=2)
-#         self.sigmoid = nn.Sigmoid()
-#
-#         self.conv_q_left = nn.Conv2d(self.inplanes, self.inter_planes, kernel_size=1, stride=stride, padding=0, bias=False)   #g
-#         self.avg_pool = nn.AdaptiveAvgPool2d(1)
-#         self.conv_v_left = nn.Conv2d(self.inplanes, self.inter_planes, kernel_size=1, stride=stride, padding=0, bias=False)   #theta
-#         self.softmax_left = nn.Softmax(dim=2)
-#
-#         self.reset_parameters()
-#
-#     def reset_parameters(self):
-#         kaiming_init(self.conv_q_right, mode='fan_in')
-#         kaiming_init(self.conv_v_right, mode='fan_in')
-#         kaiming_init(self.conv_q_left, mode='fan_in')
-#         kaiming_init(self.conv_v_left, mode='fan_in')
-#
-#         self.conv_q_right.inited = True
-#         self.conv_v_right.inited = True
-#         self.conv_q_left.inited = True
-#         self.conv_v_left.inited = True
-#
-#     def spatial_pool(self, x):
-#         input_x = self.conv_v_right(x)
-#
-#         batch, channel, height, width = input_x.size()
-#
-#         # [N, IC, H*W]
-#         input_x = input_x.view(batch, channel, height * width)
-#
-#         # [N, 1, H, W]
-#         context_mask = self.conv_q_right(x)
-#
-#         # [N, 1, H*W]
-#         context_mask = context_mask.view(batch, 1, height * width)
-#
-#         # [N, 1, H*W]
-#         context_mask = self.softmax_right(context_mask)
-#
-#         # [N, IC, 1]
-#         # context = torch.einsum('ndw,new->nde', input_x, context_mask)
-#         context = torch.matmul(input_x, context_mask.transpose(1,2))
-#         # [N, IC, 1, 1]
-#         context = context.unsqueeze(-1)
-#
-#         # [N, OC, 1, 1]
-#         context = self.conv_up(context)
-#
-#         # [N, OC, 1, 1]
-#         mask_ch = self.sigmoid(context)
-#
-#         out = x * mask_ch
-#
-#         return out
-#
-#     def channel_pool(self, x):
-#         # [N, IC, H, W]
-#         g_x = self.conv_q_left(x)
-#
-#         batch, channel, height, width = g_x.size()
-#
-#         # [N, IC, 1, 1]
-#         avg_x = self.avg_pool(g_x)
-#
-#         batch, channel, avg_x_h, avg_x_w = avg_x.size()
-#
-#         # [N, 1, IC]
-#         avg_x = avg_x.view(batch, channel, avg_x_h * avg_x_w).permute(0, 2, 1)
-#
-#         # [N, IC, H*W]
-#         theta_x = self.conv_v_left(x).view(batch, self.inter_planes, height * width)
-#
-#         # [N, 1, H*W]
-#         # context = torch.einsum('nde,new->ndw', avg_x, theta_x)
-#         context = torch.matmul(avg_x, theta_x)
-#         # [N, 1, H*W]
-#         context = self.softmax_left(context)
-#
-#         # [N, 1, H, W]
-#         context = context.view(batch, 1, height, width)
-#
-#         # [N, 1, H, W]
-#         mask_sp = self.sigmoid(context)
-#
-#         out = x * mask_sp
-#
-#         return out
-#
-#     def forward(self, x):
-#         # [N, C, H, W]
-#         context_channel = self.spatial_pool(x)
-#         # [N, C, H, W]
-#         context_spatial = self.channel_pool(x)
-#         # [N, C, H, W]
-#         out = context_spatial + context_channel
-#         return out
-class PSA_s(nn.Module):
-    def __init__(self, inplanes, planes, kernel_size=1, stride=1):
-        super(PSA_s, self).__init__()
-
-        self.inplanes = inplanes
-        self.inter_planes = planes // 2
-        self.planes = planes
-        self.kernel_size = kernel_size
-        self.stride = stride
-        self.padding = (kernel_size - 1) // 2
-        ratio = 4
-
-        self.conv_q_right = nn.Conv2d(self.inplanes, 1, kernel_size=1, stride=stride, padding=0, bias=False)
-        self.conv_v_right = nn.Conv2d(self.inplanes, self.inter_planes, kernel_size=1, stride=stride, padding=0,
-                                      bias=False)
-        # self.conv_up = nn.Conv2d(self.inter_planes, self.planes, kernel_size=1, stride=1, padding=0, bias=False)
-        self.conv_up = nn.Sequential(
-            nn.Conv2d(self.inter_planes, self.inter_planes // ratio, kernel_size=1),
-            nn.LayerNorm([self.inter_planes // ratio, 1, 1]),
-            nn.ReLU(inplace=True),
-            nn.Conv2d(self.inter_planes // ratio, self.planes, kernel_size=1)
-        )
-        self.softmax_right = nn.Softmax(dim=2)
-        self.sigmoid = nn.Sigmoid()
-
-        self.conv_q_left = nn.Conv2d(self.inplanes, self.inter_planes, kernel_size=1, stride=stride, padding=0,
-                                     bias=False)  # g
-        self.avg_pool = nn.AdaptiveAvgPool2d(1)
-        self.conv_v_left = nn.Conv2d(self.inplanes, self.inter_planes, kernel_size=1, stride=stride, padding=0,
-                                     bias=False)  # theta
-        self.softmax_left = nn.Softmax(dim=2)
-
-        self.reset_parameters()
-
-    def reset_parameters(self):
-        kaiming_init(self.conv_q_right, mode='fan_in')
-        kaiming_init(self.conv_v_right, mode='fan_in')
-        kaiming_init(self.conv_q_left, mode='fan_in')
-        kaiming_init(self.conv_v_left, mode='fan_in')
-
-        self.conv_q_right.inited = True
-        self.conv_v_right.inited = True
-        self.conv_q_left.inited = True
-        self.conv_v_left.inited = True
-
-    def spatial_pool(self, x):
-        input_x = self.conv_v_right(x)
-
-        batch, channel, height, width = input_x.size()
-
-        # [N, IC, H*W]
-        input_x = input_x.view(batch, channel, height * width)
-
-        # [N, 1, H, W]
-        context_mask = self.conv_q_right(x)
-
-        # [N, 1, H*W]
-        context_mask = context_mask.view(batch, 1, height * width)
-
-        # [N, 1, H*W]
-        context_mask = self.softmax_right(context_mask)
-
-        # [N, IC, 1]
-        # context = torch.einsum('ndw,new->nde', input_x, context_mask)
-        context = torch.matmul(input_x, context_mask.transpose(1, 2))
-
-        # [N, IC, 1, 1]
-        context = context.unsqueeze(-1)
-
-        # [N, OC, 1, 1]
-        context = self.conv_up(context)
-
-        # [N, OC, 1, 1]
-        mask_ch = self.sigmoid(context)
-
-        out = x * mask_ch
-
-        return out
-
-    def channel_pool(self, x):
-        # [N, IC, H, W]
-        g_x = self.conv_q_left(x)
-
-        batch, channel, height, width = g_x.size()
-
-        # [N, IC, 1, 1]
-        avg_x = self.avg_pool(g_x)
-
-        batch, channel, avg_x_h, avg_x_w = avg_x.size()
-
-        # [N, 1, IC]
-        avg_x = avg_x.view(batch, channel, avg_x_h * avg_x_w).permute(0, 2, 1)
-
-        # [N, IC, H*W]
-        theta_x = self.conv_v_left(x).view(batch, self.inter_planes, height * width)
-
-        # [N, IC, H*W]
-        theta_x = self.softmax_left(theta_x)
-
-        # [N, 1, H*W]
-        # context = torch.einsum('nde,new->ndw', avg_x, theta_x)
-        context = torch.matmul(avg_x, theta_x)
-
-        # [N, 1, H, W]
-        context = context.view(batch, 1, height, width)
-
-        # [N, 1, H, W]
-        mask_sp = self.sigmoid(context)
-
-        out = x * mask_sp
-
-        return out
-
-    def forward(self, x):
-        # [N, C, H, W]
-        out = self.spatial_pool(x)
-
-        # [N, C, H, W]
-        out = self.channel_pool(out)
-
-        # [N, C, H, W]
-        # out = context_spatial + context_channel
-
-        return out
 
 
 class AA_(nn.Module):
